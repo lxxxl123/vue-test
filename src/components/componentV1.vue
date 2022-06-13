ad2f2d1e7f1af776df4000ff69c44fda56365eda
chenwh3 <chenwh3@1234>
2022-06-13 18:16:48 +0800

    init

---
+++ src/components/componentV1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ msg1 }}
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'componentV1',
+  props: {
+    msg1: {
+      type: String,
+      default: () => "default"
+    },
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
